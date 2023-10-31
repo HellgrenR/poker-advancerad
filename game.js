13a8784c8d5725f2975a94bbc2bb9fecfac24e80
HellgrenR <rallenolltva@gmail.com>
2023-10-31 13:59:45 +0100

    addPlayers method in Game class
    added a way to add players through the game class

--- game.js
+++ game.js
@@ -1,14 +1,26 @@
 import Dealer from "./dealer.js"
+import Player from "./player.js"
 
 export default class Game {
 
+  players = []
+
   constructor() {
-    this.players = []
     this.dealer = new Dealer()
   }
 
   startGame() {
 
+  }
+
+  addPlayers(...names) {
+    if (names.length < 2) {
+      console.log("Du måste minst ha två spelare")
+    } else {
+      for (i = 0; i < names.length; i++) {
+        this.players.push(new Player(names[i]))
+      }
+    }
   }
 
 }
