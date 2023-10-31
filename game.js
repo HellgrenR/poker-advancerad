54d98186f183d9123001aedfb2e92fea25a42c15
HellgrenR <rallenolltva@gmail.com>
2023-10-31 14:28:57 +0100

    Minor fix

--- game.js
+++ game.js
@@ -23,7 +23,7 @@ export default class Game {
     if (names.length < 2) {
       console.log("Du måste minst ha två spelare")
     } else {
-      for (i = 0; i < names.length; i++) {
+      for (let i = 0; i < names.length; i++) {
         this.players.push(new Player(names[i]))
       }
     }
