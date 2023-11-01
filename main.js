99bc202658fbe819d18819dc5cd3abc9269ba776
HellgrenR <rallenolltva@gmail.com>
2023-11-01 08:57:19 +0100

    Using Game class in main.js
    Per instructions of assignment

--- main.js
+++ main.js
@@ -1,34 +1,8 @@
-import Dealer from "./dealer.js"
-import Player from "./player.js"
-import Valid from "./validation.js"
+import Game from "./game.js"
 
-let dealer = new Dealer()
+let game = new Game()
 
-let player = []
-player.push(new Player("Slim"))
-player.push(new Player("Luke"))
-
-
-dealer.randomizeCards()
-console.log(dealer.deck.cards)
-
-
-dealer.giveFiveCards(player[0])
-dealer.giveFiveCards(player[1])
-
-console.log(dealer.deck.cards.length)
-console.log(player[0].hand)
-console.log(player[1].hand)
-
-
-
-
-console.log(Valid.validate(player[0]))
-console.log(Valid.validate(player[1]))
-
-
-
-
+game.startGame()
 
 
 
